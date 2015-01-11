7bdbdb19009c60fb867882ad85089bf350aee0f6
silvia <suecarbajal@gmail.com>
2015-01-11 08:23:23 -0500

    adding

---
+++ CS280/RecursiveParsing/main.cpp
@@ -0,0 +1,34 @@
+
+/* main.cc */
+
+#include <iostream>
+#include "complex.h"
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+
+    complex a(1, 2);
+    complex b(2, 3);
+
+    int i = 5;
+
+    cout << a << " + " << b << " = " << a + b << endl;
+    cout << a << " - " << b << " = " << a - b << endl;
+    cout << a << " * " << b << " = " << a * b << endl;
+    cout << a << " / " << b << " = " << a / b << endl;
+
+    cout << a << " + " << i << " = " << a + i << endl;
+    cout << a << " - " << i << " = " << a - i << endl;
+    cout << a << " * " << i << " = " << a * i << endl;
+    cout << a << " / " << i << " = " << a / i << endl;
+
+    cout << i << " + " << a << " = " << i + a << endl;
+    cout << i << " - " << a << " = " << i - a << endl;
+    cout << i << " * " << a << " = " << i * a << endl;
+    cout << i << " / " << a << " = " << i / a << endl;
+
+    return 0;
+}
+
