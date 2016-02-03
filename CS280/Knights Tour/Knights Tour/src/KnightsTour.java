ee790724b2e0a042ecd438c99f572cef18eda605
silvia <suecarbajal@gmail.com>
2016-02-03 03:52:25 -0500

    adding knights tour

--- CS280/Knights Tour/Knights Tour/src/KnightsTour.java
+++ CS280/Knights Tour/Knights Tour/src/KnightsTour.java
@@ -6,7 +6,8 @@ public class KnightsTour {
 final static int N=8;
 static int board[][];
 
-	public static void main(String[] args){
+	public static void main(String[] args)
+	{
 		board = new int[N][N];
         int x , y;
         for (x = 0; x < N; x++) //init board to 0 (not visited)
@@ -22,7 +23,9 @@ static int board[][];
         	{
         		System.out.println();
         		for ( y = 0; y < N; y++)
-               		{System.out.print(board[x][y]+" ");}
+               		{
+               			System.out.print(board[x][y]+" ");
+               		}
         	}//end of outer for loop
     }//end of main
 	
@@ -35,39 +38,50 @@ static int board[][];
 	 */
 	
 	public static boolean move(int x, int y,int num){
-		if (x<0 || x>=8 || y<0 || y>=8 ){ //out of bound check
+		if (x<0 || x>=8 || y<0 || y>=8 )
+		{ //out of bound check
 			return false;
-			}
-		if(board[x][y]!=0){ // if the square has been visited
+		}
+		if(board[x][y]!=0)
+		{ // if the square has been visited
 			return false;
-			}
+		}
 		board[x][y]=num;//sets square to num 
 		if (num==64) //reaches end  
-			{return true;}
-		
+		{
+			return true;
+		}		
    		//all 8 moves of the knight
-		if(move(x+2, y+1,num+1)){
+		if(move(x+2, y+1,num+1))
+		{
 			return true;
 		}
-		else if(move(x+1,y-2,num+1)){
+		else if(move(x+1,y-2,num+1))
+		{
 			return true;
 		}
-		else if(move(x+1,y+2,num+1)){
+		else if(move(x+1,y+2,num+1))
+		{
 			return true;
 		}
-		else if(move(x-1,y+2,num+1)){
+		else if(move(x-1,y+2,num+1))
+		{
 			return true;
 		}
-		else if (move(x-2,y-1,num+1)){
+		else if (move(x-2,y-1,num+1))
+		{
 			return true;
 		}
-		else if(move(x-2,y+1,num+1)){
+		else if(move(x-2,y+1,num+1))
+		{
 			return true;
 		}
-		else if (move(x-1,y-2,num+1)){
+		else if (move(x-1,y-2,num+1))
+		{
 			return true;
 		}
-		else if (move(x+2,y-1,num+1)){
+		else if (move(x+2,y-1,num+1))
+		{
 			return true;
 		}
 		board[x][y]=0; // set square to 0 (not visited)
--- CS280/Knights Tour/KnightsTour.cpp
+++ CS280/Knights Tour/KnightsTour.cpp
@@ -7,40 +7,52 @@ const int N = 8;
 //#DEFINE N 8
 int board [N][N];
 
-bool move(int x, int y,int num){
-	if (x<0 || x>=8 || y<0 || y>=8 ){ //out of bound check
+bool move(int x, int y,int num)
+{
+	if (x<0 || x>=8 || y<0 || y>=8 )
+	{ //out of bound check
 		return false;
 	}
-	if(board[x][y]!=0){ // if the square has been visited
+	if(board[x][y]!=0)
+	{ // if the square has been visited
 	   	return false;
 	}			
 	board[x][y]=num;//sets square to num 		
 	if (num==64) //reaches end  
-	{return true;}	
+	{
+		return true;
+	}	
     
     //all 8 moves of the knight
-	if(move(x+2, y+1,num+1)){
+	if(move(x+2, y+1,num+1))
+	{
 		return true;
 	}
-	else if(move(x+1,y-2,num+1)){
+	else if(move(x+1,y-2,num+1))
+	{
 	   	return true;
 	}
-	else if(move(x+1,y+2,num+1)){
+	else if(move(x+1,y+2,num+1))
+	{
 	   	return true;
 	}
-	else if(move(x-1,y+2,num+1)){
+	else if(move(x-1,y+2,num+1))
+	{
 	   	return true;
 	}
-	else if (move(x-2,y-1,num+1)){
+	else if (move(x-2,y-1,num+1))
+	{
 	   	return true;
 	}
-	else if(move(x-2,y+1,num+1)){
+	else if(move(x-2,y+1,num+1))
+	{
 	   	return true;
 	}
 	else if (move(x-1,y-2,num+1)){
 	   	return true;
 	}
-	else if (move(x+2,y-1,num+1)){
+	else if (move(x+2,y-1,num+1))
+	{
 	   	return true;
 	}
 	board[x][y]=0; // set square to 0 (not visited)
@@ -49,8 +61,10 @@ bool move(int x, int y,int num){
 
 int main(){
 	int x,y;
-	for(x=0;x< N; x++){
-		for(y=0;y< N;y++){
+	for(x=0;x< N; x++)
+	{
+		for(y=0;y< N;y++)
+		{
 			 board[x][y]=0;
 		}
 	}
@@ -59,7 +73,9 @@ int main(){
 		{
         	cout << '\n';
          	for ( y = 0; y < N; y++)
-                { cout << board[x][y] << " ";}
+                { 
+                	cout << board[x][y] << " ";
+                }
         }//end of outer for loop
 	return 0;
 }//end of main
